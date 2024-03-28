dc83495fca3079d89885de4ab1b3cc1040c7a04e
ruslanbelchenkov <troum@outlook.com>
2024-03-28 18:32:01 +0300

    Uopdate

--- src/config/useGradient.ts
+++ src/config/useGradient.ts
@@ -3,9 +3,9 @@ export function useGradient (season: string) {
     case 'w':
       return 'to top right, rgba(255, 255, 255, .1), rgba( 125, 87, 278, .15)'
     case 's':
-      return 'to top right, rgba(255, 255, 255, .1), rgba( 255, 207, 87, .75)'
+      return 'to top right, rgba(255, 255, 255, .1), rgba( 227, 114, 27, .75)'
     case 'a':
-      return 'to top right, rgba(125, 87, 278, .15), rgba( 255, 207, 87, .75)'
+      return 'to top right, rgba(125, 87, 278, .15), rgba(125, 87, 278, .35), rgba( 227, 114, 27, .85)'
     default:
       return ''
   }
