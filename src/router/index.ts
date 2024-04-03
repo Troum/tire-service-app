0832d7bceaa554dc67e220039bd6cad31854ad20
ruslanbelchenkov <troum@outlook.com>
2024-04-03 16:55:17 +0300

    Update

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,9 @@
  *
  * Automatic routes for `./src/pages/*.vue`
  */
-
+// @ts-ignore
 import { createRouter, createWebHistory } from 'vue-router/auto'
+// @ts-ignore
 import { setupLayouts } from 'virtual:generated-layouts'
 import {useAppStore} from "../stores/app";
 
@@ -26,6 +27,9 @@ router.beforeEach((to: any, from: any, next: any)=> {
   const appStore = useAppStore();
   if (to.path !== '/login' && !appStore.authToken) {
     return next({path: "/login"});
+  }
+  if (to.path === '/' && appStore.authToken) {
+    return next({path: "/sizes"});
   }
   return next();
 })
