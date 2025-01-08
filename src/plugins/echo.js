9d1e8b8482ed7aea4d5e23ca477c7134fc1186ef
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2025-01-09 00:02:33 +0300

    Changes

--- src/plugins/echo.js
+++ src/plugins/echo.js
@@ -9,6 +9,7 @@ export default {
       key: import.meta.env.VITE_REVERB_APP_KEY,
       wsHost: import.meta.env.VITE_WS_URL,
       wsPort: import.meta.env.VITE_WS_PORT,
+      wssPort: import.meta.env.VITE_WSS_PORT,
       enableTransports: ['ws', 'wss'],
       forceTLS: false,
       withCredentials: false
