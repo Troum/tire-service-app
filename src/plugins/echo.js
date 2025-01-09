4ec15f10c67a53e9ccd52577ef6515287803d773
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2025-01-09 10:32:23 +0300

    Changes

--- src/plugins/echo.js
+++ src/plugins/echo.js
@@ -1,9 +1,8 @@
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
-
-window.Pusher = Pusher
 export default {
   install: (app) => {
+    window.Pusher = Pusher
     const echo = new Echo({
       broadcaster: 'reverb',
       key: import.meta.env.VITE_REVERB_APP_KEY,
