9869c4ebe76f8095a2ee3427a32a18af136190d4
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2025-07-10 15:29:38 +0300

    Changes

--- src/plugins/echo.js
+++ src/plugins/echo.js
@@ -4,14 +4,21 @@ export default {
   install: (app) => {
     window.Pusher = Pusher
     const echo = new Echo({
-      broadcaster: 'reverb',
-      key: import.meta.env.VITE_REVERB_APP_KEY,
-      wsHost: import.meta.env.VITE_WS_URL,
-      wsPort: import.meta.env.VITE_WS_PORT,
-      wssPort: import.meta.env.VITE_WSS_PORT,
-      enableTransports: ['ws', 'wss'],
-      forceTLS: false,
-      withCredentials: false
+      broadcaster: 'pusher',
+      // key: import.meta.env.VITE_REVERB_APP_KEY,
+      // wsHost: import.meta.env.VITE_WS_URL,
+      // wsPort: import.meta.env.VITE_WS_PORT,
+      // wssPort: import.meta.env.VITE_WSS_PORT,
+      // enableTransports: ['ws', 'wss'],
+      // forceTLS: false,
+      // withCredentials: false
+      key: import.meta.env.VITE_PUSHER_APP_KEY,
+      cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+      forceTLS: true,
+      wsHost: import.meta.env.VITE_PUSHER_HOST,
+      wsPort: import.meta.env.VITE_PUSHER_PORT,
+      wssPort: import.meta.env.VITE_PUSHER_PORT,
+      enabledTransports: ["ws", "wss"],
     })
 
     app.provide("echo", echo);
