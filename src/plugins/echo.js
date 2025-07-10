a5767974ec41f8ffeb51e81aec30d1894212d2b0
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2025-07-10 21:02:40 +0300

    Changes

--- src/plugins/echo.js
+++ src/plugins/echo.js
@@ -3,7 +3,7 @@ import Pusher from "pusher-js";
 export default {
   install: (app) => {
     window.Pusher = Pusher
-    const options = import.meta.env.MODE === 'development' ? {
+    const options = {
       broadcaster: 'reverb',
       key: import.meta.env.VITE_REVERB_APP_KEY,
       wsHost: import.meta.env.VITE_WS_URL,
@@ -12,16 +12,7 @@ export default {
       enableTransports: ['ws', 'wss'],
       forceTLS: false,
       withCredentials: false
-    } : {
-      broadcaster: 'pusher',
-      key: import.meta.env.VITE_PUSHER_APP_KEY,
-      cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-      forceTLS: true,
-      wsHost: import.meta.env.VITE_PUSHER_HOST,
-      wsPort: import.meta.env.VITE_PUSHER_PORT,
-      wssPort: import.meta.env.VITE_PUSHER_PORT,
-      enabledTransports: ["ws", "wss"],
-    }
+    };
     const echo = new Echo(options)
 
     app.provide("echo", echo);
